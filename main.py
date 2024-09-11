dc59d138db61bf06d1f0c89ee5d8e3586fa05c07
Sarce22 <sebastianarce482@gmail.com>
2024-09-10 20:55:42 -0500

    addMain

---
+++ main.py
@@ -0,0 +1,14 @@
+"""Esto hace una suma de dos números"""
+
+
+def suma(num1, num2):
+    """
+    Calculates sum of two numbers
+
+    Args:
+        num1 (int): first number
+        num2 (int): second number
+    Returns:
+        int: sum of num1 and num2
+    """
+    return num1 + num2
